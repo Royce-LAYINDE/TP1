d3fd63073dc03fd0991545722bcdab833719cdf6
Royce-LAYINDE <layinderoyce@gmail.com>
2023-05-06 13:46:37 +0200

    Ajout de mon premier code

---
+++ somme.py
@@ -0,0 +1,7 @@
+#Trouvew lq somme de deux no,bres
+nbr1=input("Entrer le premier nombre: ")
+nbr2=input("Entrer le deuxieme nombre: ")
+#Additionner les deux nombres
+s=int(nbr1) + int(nbr2)
+#Afficher la somme
+print("La somme de {0} et {1} est {2}".format(nbr1,nbr2, s))
